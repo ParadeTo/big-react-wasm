a97f8211be2e5142ce428d66c2a6d6a78b8a9217
youxingzhi <xingzhi.you@shopee.com>
2024-04-15 20:28:00 +0800

    blog-5 react-dom JsValue to Node

--- packages/react-dom/src/host_config.rs
+++ packages/react-dom/src/host_config.rs
@@ -1,7 +1,6 @@
 use std::any::Any;
 use std::rc::Rc;
 
-use wasm_bindgen::{JsCast, JsValue};
 use web_sys::{Node, window};
 
 use react_reconciler::HostConfig;
@@ -14,7 +13,7 @@ impl HostConfig for ReactDomHostConfig {
     fn create_text_instance(&self, content: String) -> Rc<dyn Any> {
         let window = window().expect("no global `window` exists");
         let document = window.document().expect("should have a document on window");
-        Rc::new(JsValue::from(document.create_text_node(content.as_str())))
+        Rc::new(Node::from(document.create_text_node(content.as_str())))
     }
 
     fn create_instance(&self, _type: String) -> Rc<dyn Any> {
@@ -22,18 +21,18 @@ impl HostConfig for ReactDomHostConfig {
         let document = window.document().expect("should have a document on window");
         match document.create_element(_type.as_ref()) {
             Ok(element) => {
-                Rc::new(JsValue::from(element))
+                Rc::new(Node::from(element))
             }
             Err(_) => todo!(),
         }
     }
 
     fn append_initial_child(&self, parent: Rc<dyn Any>, child: Rc<dyn Any>) {
-        let p = parent.clone().downcast::<JsValue>().unwrap();
-        let c = child.clone().downcast::<JsValue>().unwrap();
-        let p = p.dyn_ref::<Node>().unwrap();
-        let c = c.dyn_ref::<Node>().unwrap();
-        match p.append_child(c) {
+        let p = parent.clone().downcast::<Node>().unwrap();
+        let c = child.clone().downcast::<Node>().unwrap();
+        // let p = p.dyn_ref::<Node>().unwrap();
+        // let c = c.dyn_ref::<Node>().unwrap();
+        match p.append_child(&c) {
             Ok(_) => {
                 log!("append_initial_child successfully jsvalue {:?} {:?}", p, child);
             }
