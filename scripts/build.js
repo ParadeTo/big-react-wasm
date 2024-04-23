5b2f941caaa913592f0b85f159773f37c2480a91
youxingzhi <xingzhi.you@shopee.com>
2024-04-23 18:29:58 +0800

    blog-9 format build.js

--- scripts/build.js
+++ scripts/build.js
@@ -7,19 +7,43 @@ const isTest = process.argv[2] === '--test'
 
 execSync('rm -rf dist')
 
-execSync(`wasm-pack build packages/react --out-dir ${cwd}/dist/react --out-name jsx-dev-runtime ${isTest ? '--target nodejs' : ''}`)
-execSync(`wasm-pack build packages/react --out-dir ${cwd}/dist/react --out-name index ${isTest ? '--target nodejs' : ''}`)
-execSync(`wasm-pack build packages/react-dom --out-dir ${cwd}/dist/react-dom --out-name index ${isTest ? '--target nodejs' : ''}`)
+execSync(
+  `wasm-pack build packages/react --out-dir ${cwd}/dist/react --out-name jsx-dev-runtime ${
+    isTest ? '--target nodejs' : ''
+  }`
+)
+execSync(
+  `wasm-pack build packages/react --out-dir ${cwd}/dist/react --out-name index ${
+    isTest ? '--target nodejs' : ''
+  }`
+)
+execSync(
+  `wasm-pack build packages/react-dom --out-dir ${cwd}/dist/react-dom --out-name index ${
+    isTest ? '--target nodejs' : ''
+  }`
+)
 
 // modify react/package.json
 const packageJsonFilename = `${cwd}/dist/react/package.json`
-const packageJson = JSON.parse(fs.readFileSync(packageJsonFilename).toString("utf-8"))
-packageJson.files.push('jsx-dev-runtime.wasm', 'jsx-dev-runtime.js', 'jsx-dev-runtime_bg.js', 'jsx-dev-runtime_bg.wasm')
+const packageJson = JSON.parse(
+  fs.readFileSync(packageJsonFilename).toString('utf-8')
+)
+packageJson.files.push(
+  'jsx-dev-runtime.wasm',
+  'jsx-dev-runtime.js',
+  'jsx-dev-runtime_bg.js',
+  'jsx-dev-runtime_bg.wasm'
+)
 fs.writeFileSync(packageJsonFilename, JSON.stringify(packageJson))
 
 // modify react-dom/index_bg.js
-const reactDomIndexFilename = isTest ? `${cwd}/dist/react-dom/index.js` : `${cwd}/dist/react-dom/index_bg.js`
+const reactDomIndexFilename = isTest
+  ? `${cwd}/dist/react-dom/index.js`
+  : `${cwd}/dist/react-dom/index_bg.js`
 const reactDomIndexBgData = fs.readFileSync(reactDomIndexFilename)
-fs.writeFileSync(reactDomIndexFilename, (isTest ? 'const {updateDispatcher} = require("react");\n' : 'import {updateDispatcher} from "react";\n') + reactDomIndexBgData)
-
-
+fs.writeFileSync(
+  reactDomIndexFilename,
+  (isTest
+    ? 'const {updateDispatcher} = require("react");\n'
+    : 'import {updateDispatcher} from "react";\n') + reactDomIndexBgData
+)
